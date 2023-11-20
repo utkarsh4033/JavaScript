a46c5038c09039011c15b68643748968db7ebbb0
Utkarsh Sharma <109990318+utkarsh4033@users.noreply.github.com>
2023-11-20 19:54:50 +0000

    commit is done

---
+++ js-basics/datatypes.js
@@ -0,0 +1,14 @@
+"use.strict"
+// use new version synax and techniques of javascript and node
+// mdn tc39 
+// preemptive datatypes
+
+// NUmber => Integer value 
+// bigInt => big integer value
+// string => ""
+// boolean => true/false
+// undefined => value is not defined
+// symbol => for unique values
+// null => Stand alone value
+//  object 
+//  null is object
---
+++ js-basics/variables.js
@@ -0,0 +1,15 @@
+// they remain constant throughout the execution of program 
+const accountId = 12344
+// assuming the value to be assigned
+let accountEmaiul = "jhvcusd@gmail.com"
+// thier value might cahnge in the future
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountDetails;
+
+
+// prefer to not use var beacause of functional scope and block scope
+
+console.log(accountId);
+// print the current vaalue
+console.table([accountEmaiul,accountPassword,accountCity,accountDetails]);
