130d57e6540dd3ddd4282cda3b61882b76daee73
Utkarsh Sharma <109990318+utkarsh4033@users.noreply.github.com>
2024-06-14 19:35:17 +0000

    arrows

---
+++ js-basic3/arrow.js
@@ -0,0 +1,38 @@
+// const user = {
+//     username : "utkarsh",
+//     id : "burgvu",
+//     welcomeMeassage:function(){
+//         console.log(`${this.username}, is logged in`)
+//         // console.log(this);
+//     },
+// }
+// user.welcomeMeassage();
+// user.username = "sam"
+// user.welcomeMeassage();
+// console.log(this);
+//  ***************************************NORMAL FUNCTIONS*****************************************************
+// function one(){
+//     let username  = "utkarsh"
+
+//  console.log(this.username);
+// } 
+// one()
+
+// const user = function(){
+//     let username  = " utkarsh"
+//     console.log(this .username);
+// }
+// user()
+
+// ********************************************ARROW FUNCTIONS*********************************************************
+// const user1 = function(){
+//     let username  = " utkarsh"
+//     console.log(this .username);
+// }
+// user1()
+
+// const add = (num1, num2) => num1 + num2 
+// const add = (num1, num2) => (num1 + num2)
+
+// const add = (num1, num2) => ({username: "utkarsh"})
+// console.log(add(3 , 4))
